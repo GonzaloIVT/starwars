1ab52da9d4f2b366cdc693ceee8a0caceccd35b9
alonatt <alondra08@ug.uchile.cl>
2021-08-18 01:34:34 +0000

    parte2

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -9,7 +9,16 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!</h1>
-			<p />
+			<ul>
+				{store.people.map((persona, posicion) => {
+					return (
+						<li key={posicion}>
+							{" "}
+							Usuario ID {persona.uid} de nombre {persona.name}
+						</li>
+					);
+				})}
+			</ul>
 			<a href="#" className="btn btn-success">
 				If you see this green button, bootstrap is working
 			</a>
