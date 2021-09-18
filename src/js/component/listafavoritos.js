0dbbb1536a8e564e0faaba3877de26a05867ee27
alonatt <alondra08@ug.uchile.cl>
2021-09-18 22:36:18 +0000

    listo

--- src/js/component/listafavoritos.js
+++ src/js/component/listafavoritos.js
@@ -15,13 +15,17 @@ export const CardFav = () => {
 					{store.favourites.map((favorito, position) => {
 						return (
 							<DropdownItem key={position}>
-								{favorito}
-								<i
-									className="far fa-trash-alt"
-									onClick={() => {
-										actions.deleteFavourite(favorito);
-									}}
-								/>
+								<div className="row">
+									<div className="col 10">{favorito}</div>
+									<div className="col 2 justify-content-right">
+										<i
+											className="far fa-trash-alt"
+											onClick={() => {
+												actions.deleteFavourite(favorito);
+											}}
+										/>
+									</div>
+								</div>
 							</DropdownItem>
 						);
 					})}
